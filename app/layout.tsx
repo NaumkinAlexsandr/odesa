ba074a2b231acf66760150dfe6e9cc6842df4ee4
Alexander Naumkin <naumkin437@gmail.com>
2025-11-13 21:05:53 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="light" suppressHydrationWarning>
       <body
         className={` ${georgia.variable} ${caveat.variable} ${greatVibes.variable} font-sans antialiased`}
       >
